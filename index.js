9bf6d5e160e123ea1f8e36dbb4af0c8513cdcdfe
scavvv13 <scave1013@gmail.com>
2024-08-24 12:31:59 +0800

    alis

--- index.js
+++ index.js
@@ -9,10 +9,7 @@ app.use(express.json());
 
 // MongoDB connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB Atlas");
   })
